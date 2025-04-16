89ddcb76d36b4c564b8e059a2e8740720867a6b7
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-04-16 21:47:20 +0200

    feat: bot initialization

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import express, { Request, Response } from "express";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, TypeScript Express!");
+  res.send("Hello, TypeScript Express!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+  
+client.login(process.env.TOKEN);
