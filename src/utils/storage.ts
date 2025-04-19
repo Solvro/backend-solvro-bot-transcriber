bebd4613b758f04450dc9ee1a28389690b560bdd
Jakub <jkb.kobus@gmail.com>
2025-04-19 16:04:17 +0200

    feat: storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,31 @@
+interface StorageItem {
+  key: string;
+  value: any;
+}
+class Storage {
+  private data: StorageItem[] = [];
+
+  save(key: string, value: any): void {
+    const existingIndex = this.data.findIndex(item => item.key === key);
+
+    if (existingIndex !== -1)
+      this.data[existingIndex].value = value;
+    else
+      this.data.push({ key, value });
+  }
+
+  get(key: string): any | undefined {
+    const item = this.data.find(item => item.key === key);
+    return item?.value;
+  }
+
+  remove(key: string): void {
+    this.data = this.data.filter(item => item.key !== key);
+  }
+
+  clear(): void {
+    this.data = [];
+  }
+}
+
+export const storage = new Storage();
